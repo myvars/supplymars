6ae2b346d0c7f6c051a145c9b764c7023a3d2feb
myvars <adamashmore@me.com>
2024-09-20 12:26:16 +0100

    Added supplier product re-activate command

---
+++ src/Command/activateSupplierProductsCommand.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\PurchaseOrderItem;
+use App\Entity\StatusChangeLog;
+use App\Entity\Supplier;
+use App\Entity\SupplierProduct;
+use App\Enum\PurchaseOrderStatus;
+use App\Service\OrderProcessing\SupplierUtility;
+use App\Service\Product\ActiveSourceCalculator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:activate-supplier-products',
+    description: 'Activate supplier products',
+)]
+class activateSupplierProductsCommand extends Command
+{
+    public function __construct(
+        private readonly SupplierUtility $supplierUtility,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly ActiveSourceCalculator $activeSourceCalculator
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('itemCount', InputArgument::REQUIRED, 'Item count to process');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $itemCount = $input->getArgument('itemCount');
+
+        $this->supplierUtility->setDefaultUser();
+
+        $io->success('Activating supplier products');
+
+        $supplierProducts = $this->getSupplierProducts($itemCount);
+
+        $processedItemCount = 0;
+        foreach ($supplierProducts as $supplierProduct) {
+
+            $this->activeSourceCalculator->toggleStatus($supplierProduct);
+            $this->activeSourceCalculator->recalculateActiveSource($supplierProduct->getProduct());
+
+            $processedItemCount++;
+
+            $io->note(sprintf('Activating Supplier: %s, ProductCode: %d, Mapped Product ID: %d',
+                $supplierProduct->getSupplier()->getName(),
+                $supplierProduct->getId(),
+                $supplierProduct->getProduct()->getId()
+            ));
+        }
+
+        $io->success(sprintf('Processed %d Items', $processedItemCount));
+
+        return Command::SUCCESS;
+    }
+
+    public function getSupplierProducts(int $count): array
+    {
+        return $this->entityManager->getRepository(SupplierProduct::class)
+            ->findBy(['isActive' => false], null, $count);
+    }
+}
