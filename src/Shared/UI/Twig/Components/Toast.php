102ee3d2213f7c03b2a23d74974c72d31f7d2765
myvars <adamashmore@me.com>
2026-03-15 10:10:35 +0000

    Add info toast type

--- src/Shared/UI/Twig/Components/Toast.php
+++ src/Shared/UI/Twig/Components/Toast.php
@@ -13,6 +13,7 @@ final class Toast
     {
         return match ($this->type) {
             'success' => 'text-green-500 bg-green-100 dark:bg-green-800 dark:text-green-200',
+            'info' => 'text-blue-500 bg-blue-100 dark:bg-blue-800 dark:text-blue-200',
             'danger' => 'text-red-500 bg-red-100 dark:bg-red-800 dark:text-red-200',
             'warning' => 'text-orange-500 bg-orange-100 dark:bg-orange-800 dark:text-orange-200',
             default => throw new \LogicException(sprintf('Unknown toast type "%s"', $this->type)),
@@ -23,6 +24,7 @@ final class Toast
     {
         return match ($this->type) {
             'success' => 'flowbite:check-circle-solid',
+            'info' => 'flowbite:info-circle-solid',
             'danger' => 'flowbite:close-circle-solid',
             'warning' => 'flowbite:exclamation-circle-solid',
             default => throw new \LogicException(sprintf('Unknown toast icon "%s"', $this->type)),
@@ -33,6 +35,7 @@ final class Toast
     {
         return match ($this->type) {
             'success' => 'bg-green-500',
+            'info' => 'bg-blue-500',
             'danger' => 'bg-red-500',
             'warning' => 'bg-orange-500',
             default => throw new \LogicException(sprintf('Unknown Timerbar color "%s"', $this->type)),
@@ -43,6 +46,7 @@ final class Toast
     {
         return match ($this->type) {
             'success' => 3500,
+            'info' => 5000,
             'danger' => 6000,
             'warning' => 6000,
             default => 3500,
